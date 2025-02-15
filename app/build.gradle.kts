4e0e2e2f242fd38a348d39c50d27c18bb30c3210
dadachi <maurois@mac.com>
2025-02-15 20:25:09 +0900

    add test libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -138,6 +138,12 @@ dependencies {
   ksp(libs.hilt.compiler)
 
   debugImplementation(libs.androidx.compose.ui.tooling)
+
+  testImplementation(libs.androidx.navigation.testing)
+  testImplementation(libs.hilt.android.testing)
+  testImplementation(libs.kotlin.test)
+  testImplementation(libs.kotlinx.coroutines.test)
+  testImplementation(libs.robolectric)
 }
 
 
